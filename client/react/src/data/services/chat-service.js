bcc04dcf8e3b6b4abb9d705c3a3264875a1ff803
Matias Anastasio <matuanastasio@gmail.com>
2025-10-28 09:57:28 -0300

    tests usuario login

--- client/react/src/data/services/chat-service.js
+++ client/react/src/data/services/chat-service.js
@@ -1,10 +1,10 @@
-// import axios from "axios"
-// import { URL_SERVIDOR_REST } from "../../utils/configuracion"
+import axios from "axios"
+import { URL_SERVIDOR_REST } from "../../utils/configuracion"
 
-// class ChatService {
-//     async preguntar(mensaje) {
-//         return await axios.get(`${URL_SERVIDOR_REST}/ask`, { params: { question : mensaje } })
-//     }
-// }
+class ChatService {
+    async preguntar(mensaje) {
+        return await axios.get(`${URL_SERVIDOR_REST}/ask`, { params: { question : mensaje } })
+    }
+}
 
-// export const chatService = new ChatService()
+export const chatService = new ChatService()
