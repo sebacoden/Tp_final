e9a2637d4e89595984c752fdf5af286ec9e693ca
Matias Anastasio <matuanastasio@gmail.com>
2025-11-12 09:55:40 -0300

    maquetado preferencias

--- client/react/src/features/preferences/preferences.js
+++ client/react/src/features/preferences/preferences.js
@@ -3,11 +3,52 @@ import './preferences.css'
 export const Preferences = ({ cerrarPreferencias }) => {
     return (
         <div className='preferencias-block-screen' onClick={cerrarPreferencias}>
-            <div className='preferencias-container' onClick={(e) => e.stopPropagation()}>
-                <h2>User Preferences</h2>
-                <p>Here you can set your preferences.</p>
-                <button onClick={cerrarPreferencias}>Close</button>
-            </div>
+            <form className='preferencias-container' onClick={(e) => e.stopPropagation()}>
+                <h2>Mis preferencias</h2>
+                <div className='reestricciones-container'>
+                    <h3>Restricciones alimentarias</h3>
+                    <label>
+                        <input type="checkbox" name="gluten" />Celiaco (sin gluten)
+                    </label>
+                    <label>
+                        <input type="checkbox" name="lactosa" />Intolerante a la lactosa
+                    </label>
+                    <label>
+                        <input type="checkbox" name="vegano" />Vegano
+                    </label>
+                    <label>
+                        <input type="checkbox" name="vegetariano" />Vegetariano
+                    </label>
+                </div>
+                <div className='alergias-container'>
+                    <h3>Alergias</h3>
+                    <label>
+                        <input type="checkbox" name="mani" />Maní
+                    </label>
+                    <label>
+                        <input type="checkbox" name="frutos-secos" />Frutos secos
+                    </label>
+                    <label>
+                        <input type="checkbox" name="mariscos" />Mariscos
+                    </label>
+                    <label>
+                        <input type="checkbox" name="huevos" />Huevos
+                    </label>
+                    <label>
+                        <input type="checkbox" name="leche" />Leche
+                    </label>
+                    <label>
+                        <input type="checkbox" name="soja" />Soja
+                    </label>
+                    <label>
+                        <input type="checkbox" name="trigo" />Trigo
+                    </label>
+                </div>
+                <div className='preferencias-botones'>
+                    <button onClick={cerrarPreferencias}>Cancelar</button>
+                    <button type='submit' onClick={cerrarPreferencias}>Guardar</button>
+                </div>
+            </form>
         </div>
     )
 }
